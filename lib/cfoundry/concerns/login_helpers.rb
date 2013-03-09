ccb15472af7150cc5f29fd9fed8a4239b2f9dc24
Kazuto Kusama <jacopen@gmail.com>
2013-03-09 23:02:27 +0900

    Fix login_helpers to login v1 environment without UAA

--- lib/cfoundry/concerns/login_helpers.rb
+++ lib/cfoundry/concerns/login_helpers.rb
@@ -3,11 +3,23 @@ require "base64"
 module CFoundry
   module LoginHelpers
     def login_prompts
-      @base.uaa.prompts
+      if @base.uaa
+        @base.uaa.prompts
+      else
+        {
+          :username => ["text", "Email"],
+          :password => ["password", "Password"]
+        }
+      end
     end
 
     def login(username, password)
-      @base.token = AuthToken.from_uaa_token_info(@base.uaa.authorize(username, password))
+      @base.token =
+        if @base.uaa
+          AuthToken.from_uaa_token_info(@base.uaa.authorize(username, password))
+        else
+          AuthToken.new(@base.create_token({:password => password}, username)[:token])
+        end
     end
   end
 end
